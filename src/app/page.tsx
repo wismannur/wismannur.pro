4df337e4d956b59a1e1f73c441651a5accc152e8
wismannur <wismannura@gmail.com>
2024-10-30 11:25:42 +0700

    fix: some styling issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import { Typography } from "@/components/ui/Typography";
 import { Metadata } from "next";
-import Link from "next/link";
-import Image from "next/image";
+
 import MyLogo from "@/components/my-logo/MyLogo";
+import MyIntroduction from "@/components/MyIntroduction";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
@@ -12,55 +11,14 @@ export const metadata: Metadata = {
 
 const Home = () => {
   return (
-    <div className="w-full relative flex my-auto px-6 xl:px-0">
-      <div className="w-auto z-20 bg-transparent">
-        <Typography variant="h1" className="mb-4">
-          Hi!
-        </Typography>
-        <Typography variant="h1" className="mb-4">
-          You can call me <span className="text-sky-500">Wisman</span>
-        </Typography>
-        <Typography
-          variant="h4"
-          className="mb-4 bg-gradient-to-r from-sky-500 via-green-500 to-indigo-500 inline-block text-transparent bg-clip-text font-bold"
-        >
-          Frontend Web Developer
-        </Typography>
-        <Typography variant="p" className="mb-6 max-w-2xl">
-          Building websites is more than just code — it's an art. With over five
-          years in front-end development, I love designing engaging experiences
-          with React.js and Vue.js, ensuring every project looks amazing and
-          functions flawlessly.
-        </Typography>
-
-        <div className="flex space-x-4 mb-8">
-          <Link
-            href="/blog"
-            className="bg-sky-600 px-4 py-2 rounded hover:bg-sky-500"
-          >
-            <Typography variant="span" className="text-white">
-              Read the blog
-            </Typography>
-          </Link>
-          <Link
-            href="/about"
-            className="bg-gray-800 px-4 py-2 rounded hover:bg-gray-700"
-          >
-            <Typography variant="span" className="text-white">
-              Learn more about me
-            </Typography>
-          </Link>
-        </div>
-      </div>
+    <div className="w-full flex flex-col my-auto px-6 sm:px-0">
+      <MyIntroduction />
 
       <MyLogo
         classes={{
           root: clsx(
-            "absolute z-10 opacity-30 text-[120px] right-6 -bottom-16",
-            "sm:text-[200px] sm:-bottom-24",
-            "md:text-[250px] md:-bottom-32",
-            "lg:text-[300px] lg:-bottom-40",
-            "xl:text-[350px] xl:-bottom-40"
+            "opacity-30 ml-auto",
+            "text-9xl sm:text-[180px] md:text-[240px] lg:text-[280px] xl:text-[320px]"
           ),
         }}
       />
