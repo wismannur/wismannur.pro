88859d431d6ce088727be71d146a9374810bd406
wismannur <wismannura@gmail.com>
2024-10-30 13:51:14 +0700

    feat: add constants file for layout

---
+++ src/components/layout/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LIST = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+];
