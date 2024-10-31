3203adf5fb3b8a8ff899a668149ca7baab6d855b
wismannur <wismannura@gmail.com>
2024-10-31 20:37:04 +0700

    feat: setup metadata for blog, projects and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,8 @@
 import WillBeLiveSoon from "@/components/WillBeLiveSoon";
+import { metadataStatic } from "@/constants/metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = metadataStatic.about;
 
 export default function AboutPage() {
   return <WillBeLiveSoon title="About page" />;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,8 @@
 import WillBeLiveSoon from "@/components/WillBeLiveSoon";
+import { metadataStatic } from "@/constants/metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = metadataStatic.blog;
 
 export default function BlogPage() {
   return <WillBeLiveSoon title="Blog page" />;
