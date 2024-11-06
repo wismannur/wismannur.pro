5a77bdc020e78c9782d534d707e2ba149c824b49
wismannur <wismannura@gmail.com>
2024-11-06 17:00:44 +0700

    fix: umami script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "next-themes";
-import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import clsx from "clsx";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
 import Script from "next/script";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://umami.wnak.cloud/script.js"
+          data-website-id="79728430-4694-434c-8b80-853b9c596518"
+          data-domains="wismannur.pro"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body
         className={clsx(
           `${inter.className} flex flex-col min-h-screen bg-white dark:bg-black`,
@@ -32,12 +40,6 @@ export default function RootLayout({
           </main>
           <Footer />
         </ThemeProvider>
-
-        <Script
-          src="https://umami.wnak.cloud/script.js"
-          data-website-id="79728430-4694-434c-8b80-853b9c596518"
-          data-domains="wismannur.pro"
-        />
       </body>
     </html>
   );
