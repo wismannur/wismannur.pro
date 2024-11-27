9d92f3cd93f2d4c1b689aee911990e6e06eabcdb
wismannur <wismannura@gmail.com>
2024-11-27 21:10:53 +0700

    fix error build

--- src/features/about/experience.tsx
+++ src/features/about/experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Chip } from "@/components/ui/chip";
+import { Chip } from "@/components/chip";
 import { Typography } from "@/components/typography";
 
 const Experience = () => {
